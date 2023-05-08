493acb56ac5c9f0d77ef78abe5d076237637281e
Gerardo Parra <gerardoparrajuandelacruz@gmail.com>
2023-05-08 20:14:49 +0200

    Creado módulo y componente asociado al módulo.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ModuloPruebaModule } from './modulo-prueba/modulo-prueba.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ModuloPruebaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
