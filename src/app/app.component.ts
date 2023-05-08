f37a4aa36e36f09f54f3a90752ebe9717c9848f3
Gerardo Parra <gerardoparrajuandelacruz@gmail.com>
2023-05-08 19:54:19 +0200

    Implementados algunos hooks del ciclo de vida del componente.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'NuevoProyecto';
+export class AppComponent implements OnInit, OnChanges, AfterViewInit {
+  ngAfterViewInit(): void {
+    console.log('Ejecutando AfterViewInit');
+  }
+
+  titulo = 'Un título';
+  contador = 0;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Ejecutando OnChanges');
+  }
+  ngOnInit(): void {
+    console.log('Ejecutando OnInit');
+  }
+
+  doClick() {
+    this.titulo = 'Título cambiado ' + this.contador++;
+  }
 }
